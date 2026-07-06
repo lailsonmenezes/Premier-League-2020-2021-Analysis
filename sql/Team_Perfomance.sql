25fed3429f8667459c21b861036ea6d5f4612542
Lailson de Menezes <lailsonmenezes123321@gmail.com>
2026-07-06 20:10:46 -0300

    SQL files fix

--- sql/Team_Perfomance.sql
+++ sql/Team_Perfomance.sql
@@ -1,59 +1,29 @@
---
--- Arquivo gerado com SQLiteStudio v3.4.21 em ter jun 30 22:08:20 2026
---
--- Codificação de texto usada: System
---
--- Resultados da consulta:
--- WITH team_perfomance AS (
--- SELECT 
---     home_team AS Team,
---     home_team_goals AS Goals_for,
---     away_team_goals AS Goals_against,
---     1 AS Pld 
--- FROM Premier_League_2021
--- 
--- UNION ALL
--- 
--- SELECT
---     away_team as Team,
---     away_team_goals as Goals_for,
---     home_team_goals as Goals_against,
---     1 AS Pld
--- FROM Premier_League_2021
--- 
--- )
--- 
--- SELECT 
---     Team,
---     SUM(Goals_for) as GF,
---     SUM(Goals_against) as GA,
---     SUM(Pld) as Pld,
---     ROUND(SUM(Goals_for) * 1.0 / SUM(Pld), 2) AS GF_per_Game,
---     ROUND(SUM(Goals_against) * 1.0 / SUM(Pld), 2) AS GA_per_Game
--- FROM team_perfomance
--- GROUP BY Team
---
-BEGIN TRANSACTION;
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Arsenal', 55.0, 39.0, 38, 1.45, 1.03);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Aston Villa', 55.0, 46.0, 38, 1.45, 1.21);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Brighton', 40.0, 46.0, 38, 1.05, 1.21);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Burnley', 33.0, 55.0, 38, 0.87, 1.45);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Chelsea', 58.0, 36.0, 38, 1.53, 0.95);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Crystal Palace', 41.0, 66.0, 38, 1.08, 1.74);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Everton', 47.0, 48.0, 38, 1.24, 1.26);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Fulham', 27.0, 53.0, 38, 0.71, 1.39);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Leeds', 62.0, 54.0, 38, 1.63, 1.42);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Leicester', 68.0, 50.0, 38, 1.79, 1.32);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Liverpool', 68.0, 42.0, 38, 1.79, 1.11);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Man City', 83.0, 32.0, 38, 2.18, 0.84);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Man United', 73.0, 44.0, 38, 1.92, 1.16);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Newcastle', 46.0, 62.0, 38, 1.21, 1.63);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Sheffield United', 20.0, 63.0, 38, 0.53, 1.66);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Southampton', 47.0, 68.0, 38, 1.24, 1.79);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Tottenham', 68.0, 45.0, 38, 1.79, 1.18);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('West Brom', 35.0, 76.0, 38, 0.92, 2.0);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('West Ham', 62.0, 47.0, 38, 1.63, 1.24);
-INSERT INTO "Tabela Premier League 2021" (Team, GF, GA, Pld, GF_per_Game, GA_per_Game) VALUES ('Wolves', 36.0, 52.0, 38, 0.95, 1.37);
-
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+WITH
+  team_perfomance AS (
+    SELECT
+      home_team AS Team,
+      home_team_goals AS Goals_for,
+      away_team_goals AS Goals_against,
+      1 AS Pld
+    FROM
+      Premier_League_2021
+    UNION ALL
+    SELECT
+      away_team AS Team,
+      away_team_goals AS Goals_for,
+      home_team_goals AS Goals_against,
+      1 AS Pld
+    FROM
+      Premier_League_2021
+  )
+SELECT
+  Team,
+  SUM(Goals_for) AS GF,
+  SUM(Goals_against) AS GA,
+  SUM(Pld) AS Pld,
+  ROUND(SUM(Goals_for) * 1.0 / SUM(Pld), 2) AS GF_per_Game,
+  ROUND(SUM(Goals_against) * 1.0 / SUM(Pld), 2) AS GA_per_Game
+FROM
+  team_perfomance
+GROUP BY
+  Team
--- sql/Team_Statistics.sql
+++ sql/Team_Statistics.sql
@@ -1,83 +1,54 @@
---
--- Arquivo gerado com SQLiteStudio v3.4.21 em ter jun 30 22:13:58 2026
---
--- Codificação de texto usada: System
---
--- Resultados da consulta:
--- WITH teams_statistics AS (
--- SELECT
---     home_team AS Team,
---     CASE
---         WHEN result = "Win" THEN 1
---         ELSE 0
---         END as HW,
---     CASE
---         WHEN result = "Lose" THEN 1
---         ELSE 0
---         END as HL,
---     CASE
---         WHEN result = "Draw" THEN 1
---         ELSE 0
---         END as HD,
---     0 as AW,
---     0 as AL,
---     0 as AD
--- FROM Premier_League_2021
--- 
--- UNION ALL
--- 
--- SELECT
---     away_team as Team,
---     0 as HW,
---     0 as HL,
---     0 as HD,
---     CASE 
---         WHEN result = "Lose" THEN 1
---         ELSE 0
---         END as AW,
---     CASE
---         WHEN result = "Win" THEN 1
---         ELSE 0
---         END as AL,
---     CASE
---         WHEN result = "Draw" THEN 1
---         ELSE 0
---         END AS AD
--- FROM Premier_League_2021
--- )
--- 
--- SELECT
---     team as Team,
---     Sum(HW) as Home_Wins,
---     sum(HL) as Home_Losses,
---     sum(HD) as Home_Draws,
---     sum(AW) as Away_Wins,
---     sum(AL) as Away_Losses,
---     sum(AD) as Away_Draws
--- FROM teams_statistics
--- GROUP BY Team
---
-BEGIN TRANSACTION;
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Arsenal', 8, 7, 4, 10, 6, 3);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Aston Villa', 7, 8, 4, 9, 7, 3);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Brighton', 4, 6, 9, 5, 9, 5);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Burnley', 4, 9, 6, 6, 10, 3);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Chelsea', 9, 4, 6, 10, 5, 4);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Crystal Palace', 6, 8, 5, 6, 10, 3);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Everton', 6, 9, 4, 11, 4, 4);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Fulham', 2, 13, 4, 3, 7, 9);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Leeds', 8, 6, 5, 10, 9, 0);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Leicester', 9, 9, 1, 11, 3, 5);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Liverpool', 10, 6, 3, 10, 3, 6);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Man City', 13, 4, 2, 14, 2, 3);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Man United', 9, 6, 4, 12, 0, 7);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Newcastle', 6, 8, 5, 6, 9, 4);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Sheffield United', 5, 13, 1, 2, 16, 1);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Southampton', 8, 8, 3, 4, 11, 4);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Tottenham', 10, 6, 3, 8, 6, 5);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('West Brom', 3, 10, 6, 2, 12, 5);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('West Ham', 10, 5, 4, 9, 6, 4);
-INSERT INTO "Tabela Premier League 2021" (Team, Home_Wins, Home_Loses, Home_Draws, Away_Wins, Away_Loses, Away_Draws) VALUES ('Wolves', 7, 8, 4, 5, 9, 5);
-
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+WITH
+  teams_statistics AS (
+    SELECT
+      home_team AS Team,
+      CASE
+        WHEN result = "Win" THEN 1
+        ELSE 0
+      END AS HW,
+      CASE
+        WHEN result = "Lose" THEN 1
+        ELSE 0
+      END AS HL,
+      CASE
+        WHEN result = "Draw" THEN 1
+        ELSE 0
+      END AS HD,
+      0 AS AW,
+      0 AS AL,
+      0 AS AD
+    FROM
+      Premier_League_2021
+    UNION ALL
+    SELECT
+      away_team AS Team,
+      0 AS HW,
+      0 AS HL,
+      0 AS HD,
+      CASE
+        WHEN result = "Lose" THEN 1
+        ELSE 0
+      END AS AW,
+      CASE
+        WHEN result = "Win" THEN 1
+        ELSE 0
+      END AS AL,
+      CASE
+        WHEN result = "Draw" THEN 1
+        ELSE 0
+      END AS AD
+    FROM
+      Premier_League_2021
+  )
+SELECT
+  team AS Team,
+  Sum(HW) AS Home_Wins,
+  sum(HL) AS Home_Losses,
+  sum(HD) AS Home_Draws,
+  sum(AW) AS Away_Wins,
+  sum(AL) AS Away_Losses,
+  sum(AD) AS Away_Draws
+FROM
+  teams_statistics
+GROUP BY
+  Team
